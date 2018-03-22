6203a900264c5a3d6cb0f9600620dbc1db527d92
m305-43\w401 <biyori03091@gmail.com>
2018-03-22 16:51:45 +0800

    Add While

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -13,16 +13,27 @@ namespace Lottery
             // 建立亂數物件
             Random rand = new Random();
 
-            // 用亂數物件產生亂數
-            int r1 = rand.Next(1, 43);
-            int r2 = rand.Next(1, 43);
-            int r3 = rand.Next(1, 43);
-            int r4 = rand.Next(1, 43);
-            int r5 = rand.Next(1, 43);
-            int r6 = rand.Next(1, 43);
+            // 紀錄產生次數
+            int count = 1;
 
-            // 顯示
-            Console.WriteLine("號碼 : {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+            // 重複10次
+            while (count <= 11) {
+
+                // 用亂數物件產生亂數
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                int r6 = rand.Next(1, 43);
+
+                // 顯示
+                Console.WriteLine("[{0:00}] 號碼 : {1:00} {2:00} {3:00} {4:00} {5:00}", count, r1, r2, r3, r4, r5, r6);
+
+                // 累加產生次數
+                count++;
+
+                     }
 
             // 防止視窗自動關閉
             Console.ReadLine();
